8b2e321cbca0ab8f4521f2e8af0f33064545f43a
Faust <2565158031@qq.com>
2020-12-25 15:13:47 +0800

    创建项目-新建README

---
+++ main.py
@@ -0,0 +1,3 @@
+from modules.autotrack import *
+
+sample = AutoTrack()
---
+++ modules/autotrack.py
@@ -0,0 +1,15 @@
+class AutoTrack(object):
+    def __init__(self):
+        pass
+
+    def mapping(self):
+        pass
+
+    def printMap(self):
+        pass
+
+    def track(self):
+        pass
+
+    def entity(self):
+        pass
