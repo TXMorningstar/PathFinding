de7c8964ed17d03c92cbd0b20679cf94b9a922c0
Faust <2565158031@qq.com>
2020-12-29 20:37:35 +0800

    完成了最基本的寻路功能

--- modules/autotrack.py
+++ modules/autotrack.py
@@ -1,3 +1,8 @@
+# 受到python本身的限制，迭代最多进行1000次，意味着地图不能做的过大
+# 如果要使用更大的二维坐标进行计算，应修改默认的最大迭代上限
+# import sys
+# sys.setrecursionlimit(最大迭代上限)
+
 class Autotrack(object):
 
     '''
@@ -46,6 +51,39 @@ class Autotrack(object):
     def getMap(self):
         return self._map
 
+    def printinit(self):
+        #创建映射表用于打印
+        self._mappingtable = {
+        0 : self.__config.block,
+        "o" : self.__config.obstacle,
+        "s" : self.__config.start,
+        "e" : self.__config.end,
+        "l" : self.__config.left,
+        "r" : self.__config.right,
+        "u" : self.__config.up,
+        "d" : self.__config.down
+        }
+
+    # 以美观的方式打印地图
+    def printMap(self):
+        for x in range(self.__config.y):
+            for y in range(self.__config.x):
+                # 如果坐标内为整型且不为0则正常输出
+                if type(self._map[y][x]) == int and self._map[y][x] != 0:
+                    print(self._map[y][x],end=" ")
+                # 否则使用映射表输出
+                else:
+                    print(self._mappingtable[self._map[y][x]],end=" ")
+            print()
+
+    # 以程序的方式打印地图
+    def printRawMap(self):
+        for x in range(self.__config.y):
+            for y in range(self.__config.x):
+                print(self._map[y][x],end=" ")
+            print()
+
+
     '''
     与寻路相关的方法
     ----------------------------------------------------------------------------
@@ -68,7 +106,7 @@ class Autotrack(object):
         self.__open.append(start)
 
         #开始寻路
-        self.track()
+        return self.track()
 
     # 计算F G H的值
     def attr(self,pos,father):
@@ -83,6 +121,7 @@ class Autotrack(object):
             g = 0
         else:
             g = self._map[father[0]][father[1]][0][1] + 1
+
         # H值计算 —— 终点的预估距离
         h = abs(pos[0]-self._end[0]) + abs(pos[1]-self._end[1])
 
@@ -95,21 +134,12 @@ class Autotrack(object):
     # 寻路
     # father代表父节点，子节点是由父节点衍生出的节点，父节点就是上一个被检查的节点
     def track(self,father=None):
+        # 调试用 ################################################################
         print("-----------------tracking---------------------")
-        for x in range(self.__config.y):
-            for y in range(self.__config.x):
-                print(self._map[y][x],end=" ")
-            print()
+        # self.printRawMap()
 
-
-
-
-
-
-        # 取出 “一个” 节点
-
-        # 首次运行时的初始化
-        # 如果没有父节点，说明这个节点是初次运行
+        # 取出节点
+        # 首次运行时的初始化（如果没有父节点，说明这个节点是初次运行）
         if father == None:
             father = self._start
             x = self._start[0]
@@ -119,29 +149,31 @@ class Autotrack(object):
             x = father[0]
             y = father[1]
 
-
         # 扫描 “四个” 周边方块的信息，随后向坐标内保存F G H的值
         # 四个元组代表扫描的方向：上、左、下、右
         for i in (0,-1),(-1,0),(0,1),(1,0):
-            scanpt = self.scan(x+i[0],y+i[1]) #scanpt会存储被扫描的点的信息
-
+            scanpt = self.scan(x+i[0],y+i[1]) #scanpt会存储被扫描的点的数据
+            # 被检查的点是否为路径终点
+            if scanpt == "e":
+                return self.trace(father)
             # 判断:
             #   节点有无障碍、是否在close列表中
             #   现在还不是很清楚open列表是否需要检查，暂定为需要检查（跳过open列表可以减少部分计算量
-            if scanpt == 0 and scanpt not in self.__close:
+            elif scanpt == 0 and scanpt not in self.__close:
                 self.__open.append((x+i[0],y+i[1])) #将节点信息放入open列表中
-############### print("open =",self.__open)
                 self._map[x+i[0]][y+i[1]] = (self.attr((x+i[0],y+i[1]),father),(father)) #向地图内添加F G H与父坐标的信息
 
-
         # 比对节点中F值最小的一个，这一个节点最有希望成为最短路径中的一个节点
+        # 不过在开始比对之前还有一件事要做
+        if len(self.__open) == 0:
+            print("No matched way")
+            return "fail"
+        # 开始比对
         comp = {} #comp会存放所有节点对应的F值
         for i in range(len(self.__open)):
             f = self._map[self.__open[i][0]][self.__open[i][1]][0][0] #找到F的值
             comp[f] = self.__open[i] #将坐标放进comp，格式是f:坐标
-        # 开始比对
         minFpos = comp[min(comp)] #min(comp)通过字典找到了最低的f，然后以f作为key找到坐标，就算f有相同的也不影响
-
 
         # 将节点从open中移至close
         self.__open.remove(minFpos) #把这个找到的坐标从open里移出去
@@ -150,53 +182,29 @@ class Autotrack(object):
         # 继续迭代，检查下一个节点
         return self.track(minFpos)
 
-
+    # 扫描一个点，然后返回这个点内部的数据
     def scan(self,x,y):
         # 检查一下有没有超出边界，是不是障碍，如果是就不管了
-        if x >= 0 and y >= 0 and self._map[x][y] != "b":
+        if x >= 0 and y >= 0 and x < self.__config.x and y < self.__config.y and self._map[x][y] != "b":
             return self._map[x][y]
 
+    # 寻路到终点后，通过父坐标迭代返回并找到完整路径
+    def trace(self,father):
+        trace = [self._end] #终点的坐标一定是移动的最后一步，所以首先放入列表中
+        # 循环F次
+        pos = father #正在检查的坐标，每检查一个新坐标就会更新一次
+        for i in range(self._map[father[0]][father[1]][0][1]):
+            pos = self._map[pos[0]][pos[1]][1] #找到父节点，随后将下一个子节点更新为这个节点
+            trace.append(pos) #把刚刚找到的父节点记录进trace里
+
+        # 记录完成，接下来可以输出了
+        trace.reverse() #因为append记录的列表是从终点向起点走过去的，因此需要反向列表
+        return trace
 
 
 
-
-    # 操作方法
-    # 命令控制
-    def command(self):
-        pass
 
 
 class ShowMap(object):
     def __init__(self,config):
         self.__config = config
-
-        #创建映射表用于打印
-        self._mappingtable = {
-        0 : self.__config.block,
-        "o" : self.__config.obstacle,
-        "s" : self.__config.start,
-        "e" : self.__config.end,
-        "l" : self.__config.left,
-        "r" : self.__config.right,
-        "u" : self.__config.up,
-        "d" : self.__config.down
-        }
-
-    # 以美观的方式打印地图
-    def printMap(self,map):
-        for x in range(self.__config.y):
-            for y in range(self.__config.x):
-                # 如果坐标内为整型且不为0则正常输出
-                if type(map[y][x]) == int and map[y][x] != 0:
-                    print(map[y][x],end=" ")
-                # 否则使用映射表输出
-                else:
-                    print(self._mappingtable[map[y][x]],end=" ")
-            print()
-
-    # 以程序的方式打印地图
-    def printRawMap(self,map):
-        for x in range(self.__config.y):
-            for y in range(self.__config.x):
-                print(map[y][x],end=" ")
-            print()
