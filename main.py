1b890b9f8bd13da9f06dd113816574f58df16843
Faust <2565158031@qq.com>
2020-12-29 20:37:20 +0800

    主程序范例

--- main.py
+++ main.py
@@ -12,13 +12,4 @@ printer = ShowMap(config)
 # 创建一个地图，传入大小、样式
 track.map(config.x,config.y)
 track.obstacle((1,1))
-track.target((0,0),(10,10))
-track.test((0,0))
-
-# a = track.getMap()
-# print("----------------\n"*20)
-# print(a[0][0][0][1])
-
-printer.printRawMap(track.getMap())
-
-# printer.printMap(track.getMap())
+print(track.target((0,0),(10,10)))
