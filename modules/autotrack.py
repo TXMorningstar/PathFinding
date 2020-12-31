c234b66739489cdbd11d6f5ee28c769c1d621b12
Faust <2565158031@qq.com>
2020-12-31 16:53:25 +0800

    删除了多余的操作使使用更简单，修复bug

--- modules/autotrack.py
+++ modules/autotrack.py
@@ -24,10 +24,14 @@ class Autotrack(object):
 
         可以参考config.py中的设置
     '''
-    def __init__(self,config):
-        self.__config = config
+    def __init__(self,x,y,print=None):
+        self._x = x
+        self._y = y
+        self.__print = print
         self.__open = []
         self.__close = []
+        self._trace = []
+        self._map = [[0 for i in range(y)] for i in range(x)]
 
     '''
     地图相关的方法
@@ -38,10 +42,6 @@ class Autotrack(object):
     列表中被计算到的点会以元组的方式储存FGH以及父节点的值:
         (F的值,G的值,H的值,(父节点x,父节点y))
     '''
-    # 创建地图
-    def map(self,x,y):
-        self._x, self._y = x, y
-        self._map = [[0 for i in range(y)] for i in range(x)]
 
     # 传入一个turple作为坐标，将列表的对应坐标修改为障碍
     def obstacle(self,pos):
@@ -51,35 +51,56 @@ class Autotrack(object):
     def getMap(self):
         return self._map
 
-    def printinit(self):
-        #创建映射表用于打印
-        self._mappingtable = {
-        0 : self.__config.block,
-        "o" : self.__config.obstacle,
-        "s" : self.__config.start,
-        "e" : self.__config.end,
-        "l" : self.__config.left,
-        "r" : self.__config.right,
-        "u" : self.__config.up,
-        "d" : self.__config.down
-        }
-
     # 以美观的方式打印地图
     def printMap(self):
-        for x in range(self.__config.y):
-            for y in range(self.__config.x):
-                # 如果坐标内为整型且不为0则正常输出
-                if type(self._map[y][x]) == int and self._map[y][x] != 0:
-                    print(self._map[y][x],end=" ")
-                # 否则使用映射表输出
+        for x in range(self._y):
+            for y in range(self._x):
+                # 如果节点为开始坐标
+                if (y,x) == self._start:
+                    print("☆",end=" ")
+                # 如果节点是结束坐标
+                elif (y,x) == self._end:
+                    print("★",end=" ")
+                # 如果节点在open列表中
+                elif (y,x) in self.__open:
+                    print("☑ ",end=" ")
+                # 如果节点在close列表中
+                elif (y,x) in self.__close:
+                    print("☒ ",end=" ")
+                # 如果节点是障碍
+                elif self._map[y][x] == "o":
+                    print("■",end=" ")
+                # 如果节点内数据为0
                 else:
-                    print(self._mappingtable[self._map[y][x]],end=" ")
+                    print("□",end=" ")
             print()
+
+    # 适用于寻路完成后打印路线
+    def printTrack(self):
+        for x in range(self._y):
+            for y in range(self._x):
+                # 如果节点为开始坐标
+                if (y,x) == self._start:
+                    print("☆",end=" ")
+                # 如果节点是结束坐标
+                elif (y,x) == self._end:
+                    print("★",end=" ")
+                # 如果节点为路径之一
+                elif (y,x) in self._trace:
+                    print("  ",end=" ")
+                # 如果节点是障碍
+                elif self._map[y][x] == "o":
+                    print("■",end=" ")
+                # 如果节点内数据为0
+                else:
+                    print("□",end=" ")
+            print()
+
 
     # 以程序的方式打印地图
     def printRawMap(self):
-        for x in range(self.__config.y):
-            for y in range(self.__config.x):
+        for x in range(self._y):
+            for y in range(self._x):
                 print(self._map[y][x],end=" ")
             print()
 
@@ -88,7 +109,7 @@ class Autotrack(object):
     与寻路相关的方法
     ----------------------------------------------------------------------------
     实体是寻路的对象
-    传入起点、终点和步数即可寻路
+    传入起点、终点即可寻路
     寻路时会以起点为中心向外寻路，绕开所有障碍物直到到达终点
     寻路完成后，返回前进的道路的每一步的坐标(turple)和总步数
     使用printTrack方法可以打印带有路径的地图
@@ -110,7 +131,7 @@ class Autotrack(object):
 
     # 计算F G H的值
     def attr(self,pos,father):
-        print("-----------------adding attr---------------------")
+        # print("-----------------adding attr---------------------")
         # 初始化
         x = pos[0]
         y = pos[1]
@@ -135,8 +156,13 @@ class Autotrack(object):
     # father代表父节点，子节点是由父节点衍生出的节点，父节点就是上一个被检查的节点
     def track(self,father=None):
         # 调试用 ################################################################
-        print("-----------------tracking---------------------")
-        # self.printRawMap()
+        # print("-----------------tracking---------------------")
+        if self.__print == "raw":
+            self.printRawMap()
+            print("------------------------------------------------------------")
+        if self.__print == "map":
+            self.printMap()
+            print("------------------------------------------------------------")
 
         # 取出节点
         # 首次运行时的初始化（如果没有父节点，说明这个节点是初次运行）
@@ -185,18 +211,18 @@ class Autotrack(object):
     # 扫描一个点，然后返回这个点内部的数据
     def scan(self,x,y):
         # 检查一下有没有超出边界，是不是障碍，如果是就不管了
-        if x >= 0 and y >= 0 and x < self.__config.x and y < self.__config.y and self._map[x][y] != "b":
+        if x >= 0 and y >= 0 and x < self._x and y < self._y and self._map[x][y] != "b":
             return self._map[x][y]
 
     # 寻路到终点后，通过父坐标迭代返回并找到完整路径
     def trace(self,father):
-        trace = [self._end] #终点的坐标一定是移动的最后一步，所以首先放入列表中
+        self._trace = [self._end,father] #终点的坐标一定是移动的最后一步，所以首先放入列表中；父坐标不会在循环内被输入列表，因此同样放入列表
         # 循环F次
         pos = father #正在检查的坐标，每检查一个新坐标就会更新一次
         for i in range(self._map[father[0]][father[1]][0][1]):
             pos = self._map[pos[0]][pos[1]][1] #找到父节点，随后将下一个子节点更新为这个节点
-            trace.append(pos) #把刚刚找到的父节点记录进trace里
+            self._trace.append(pos) #把刚刚找到的父节点记录进trace里
 
         # 记录完成，接下来可以输出了
-        trace.reverse() #因为append记录的列表是从终点向起点走过去的，因此需要反向列表
-        return trace
+        self._trace.reverse() #因为append记录的列表是从终点向起点走过去的，因此需要反向列表
+        return self._trace
