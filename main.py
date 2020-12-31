01c5d088aacb32f0ba89d1afd50842c8be890581
Faust <2565158031@qq.com>
2020-12-31 16:54:01 +0800

    更新了主程序示例

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 '''
 
 from modules.autotrack import *
-from config import *
 
-config = Config() #实例化Config类
-track = Autotrack(config) #实例化Autotrack类，传入config类的实例
+# 实例化Autotrack类
+# "map"是可选参数，可以换成"raw"以及不填试试
+track = Autotrack(16,16,"map")
 
+# 放置障碍物
+track.obstacle((1,1))
+track.obstacle((1,0))
+track.obstacle((2,2))
+track.obstacle((5,3))
 
-track.map(config.x,config.y) #创建一个地图，传入大小、样式
-track.obstacle((1,1)) #放置一个障碍物
-print(track.target((0,0),(10,10))) #寻路，然后打印寻路结果
+# 寻路,把寻路结果赋值给一个变量
+variable = track.target((0,0),(10,10))
+
+# 打印路线
+track.printTrack()  #打印一个好看的地图到cmd窗口上
+print("Autotrack sucessed\n",variable) #在cmd窗口上打印寻路结果
