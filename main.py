33bc8041ff5ebb5c61610223353cc6d0f3501dc5
Faust <2565158031@qq.com>
2021-01-01 18:29:30 +0800

    没什么大变化

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from modules.autotrack import *
 
 # 实例化Autotrack类
 # "map"是可选参数，可以换成"raw"以及不填试试
-track = Autotrack(16,16,"map")
+track = Autotrack(64,64)
 
 # 放置障碍物
 track.obstacle((1,1))
@@ -15,7 +15,7 @@ track.obstacle((2,2))
 track.obstacle((5,3))
 
 # 寻路,把寻路结果赋值给一个变量
-variable = track.target((0,0),(10,10))
+variable = track.target((0,0),(60,60))
 
 # 打印路线
 track.printTrack()  #打印一个好看的地图到cmd窗口上
