e34bb0c9dae82692ce726c1aad6dbdf716a5df7f
Faust <2565158031@qq.com>
2021-01-02 16:14:52 +0800

    发现了bug

--- main.py
+++ main.py
@@ -6,16 +6,14 @@ from modules.autotrack import *
 
 # 实例化Autotrack类
 # "map"是可选参数，可以换成"raw"以及不填试试
-track = Autotrack(64,64)
+track = Autotrack(16,16,"map")
 
 # 放置障碍物
-track.obstacle((1,1))
-track.obstacle((1,0))
-track.obstacle((2,2))
-track.obstacle((5,3))
+for i in (1,0),(1,1),(2,3),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(4,7),(3,7),(2,7),(1,7):
+    track.obstacle(i)
 
 # 寻路,把寻路结果赋值给一个变量
-variable = track.target((0,0),(60,60))
+variable = track.target((0,0),(15,15))
 
 # 打印路线
 track.printTrack()  #打印一个好看的地图到cmd窗口上
